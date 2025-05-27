4b451ad7f91112ce8df6b9cf490974b9903f3a66
Anupam Dutta <anupam_bit2004@hotmail.com>
2025-05-27 12:47:54 +0530

    Update and rename add_subnet_existing_vpc.tf to add_subnet_in_existing_vpc.tf

--- module12_terraform/add_subnet_in_existing_vpc.tf
+++ module12_terraform/add_subnet_in_existing_vpc.tf
@@ -4,6 +4,16 @@ provider "aws" {
   secret_key = "G5Gjkn9hA1ivKeqAv/nBbozE9J3rjBP9mhUjla28"
 }
 
+resource "aws_vpc" "development-vpc"{
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1"{
+  vpc_id = aws_vpc.development-vpc.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-2a"
+} 
+
 data "aws_vpc" "existing_vpc" {
   default = true 
 }
